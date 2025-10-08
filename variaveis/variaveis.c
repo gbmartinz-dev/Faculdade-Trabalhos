6184542468e79a6eb80daf1aeafed9c11218e20d
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-10-07 21:23:16 -0300

    Aprendendo sobre variáveis e como declara-las

---
+++ variaveis/variaveis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int idade;
+  int quantidade = 1;
+  float altura = 1.63;
+  double peso = 71.5;
+  char letra = 'G';
+  char nome[20] = "Gabriel";
+
+  idade = 27;
+  quantidade = 20;
+}
